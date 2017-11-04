66e4ccef799120100df02871181bf9421ff722cd
Lubos Hricak <lubos@hricak.sk>
2017-11-04 20:03:57 +0100

    Configured Sentry

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,7 @@
+Raven.configure do |config|
+  config.async = lambda { |event| SentryJob.perform_later(event) }
+  config.environments = %w[production]
+  config.excluded_exceptions += ['Sidekiq::Shutdown']
+  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+  config.tags = { application: 'GliderPath.API' }
+end

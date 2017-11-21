0e02dbd74e9e404e06ccf093d7d011a64bfb48ee
Lubos Hricak <lubos@hricak.sk>
2017-11-21 21:40:23 +0100

    Brought clarity and order into concerns

--- app/models/concerns/has_member.rb
+++ app/models/concerns/has_member.rb
@@ -3,6 +3,11 @@ module HasMember
 
   included do
     has_one :workspace_user, as: :member, inverse_of: :member, dependent: :destroy
-    has_one :workspace, through: :workspace_user, as: :member, inverse_of: :member, foreign_key: :workspace_id, class_name: 'Workspace'
+    has_one :workspace,
+            through: :workspace_user,
+            as: :member,
+            inverse_of: :member,
+            class_name: 'Workspace',
+            foreign_key: :workspace_id
   end
 end

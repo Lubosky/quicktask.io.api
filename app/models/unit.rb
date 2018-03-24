d091d100b512586476fd2b47c187113b6c32ef1d
Lubos Hricak <lubos@hricak.sk>
2018-03-24 16:55:00 +0100

    Added `fixed` unit type

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -3,7 +3,7 @@ class Unit < ApplicationRecord
 
   belongs_to :workspace, inverse_of: :units
 
-  enum unit_type: { volume: 0, time: 1, percentage: 2 }
+  enum unit_type: { volume: 0, time: 1, percentage: 2, fixed: 3 }
 
   validates_presence_of :name
   validates_uniqueness_of :name, case_sensitive: false, scope: :workspace_id

db54327d8fbe869be7665261f65727358544f0d9
Lubos Hricak <lubos@hricak.sk>
2018-04-06 18:35:47 +0200

    Use ID as serializer's primary key

--- app/serializable/serializable_base.rb
+++ app/serializable/serializable_base.rb
@@ -2,6 +2,4 @@ class SerializableBase < JSONAPI::Serializable::Resource
   extend JSONAPI::Serializable::Resource::KeyFormat
 
   key_format ->(key) { key.to_s.camelize(:lower) }
-
-  id { @object.uuid }
 end

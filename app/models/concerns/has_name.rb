2288e15abb6dc3ac5b11d35314cb97e2f9e7fbe5
Lubos Hricak <lubos@hricak.sk>
2018-08-18 09:56:56 +0200

    FIX: :hammer: Fixed name generator

--- app/models/concerns/has_name.rb
+++ app/models/concerns/has_name.rb
@@ -4,7 +4,7 @@ module HasName
   included do
     def name
       return business_name if self.is_a?(Contractor) && self.business_name.present?
-      return [first_name, last_name].compact.join(' ').strip! if first_name.present? || last_name.present?
+      return [first_name, last_name].delete_if { |a| a.blank? }.join(' ').strip if first_name.present? || last_name.present?
       return email
     end
   end

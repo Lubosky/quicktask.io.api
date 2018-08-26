e3ddccfad998868b8994852a8ae0abadaac9d0e2
Lubos Hricak <lubos@hricak.sk>
2018-08-26 15:36:12 +0200

    FIX: Rubocop fix :robot:

--- spec/requests/content_compression_spec.rb
+++ spec/requests/content_compression_spec.rb
@@ -6,17 +6,19 @@ RSpec.describe Rack::Deflater, type: :request do
 
     valid_token_authentication
 
-    get url_for([:me, :api, :users]), headers: { 'CONTENT_TYPE' => 'application/vnd.quicktask.v1+json',
-                                            'HTTP_AUTHORIZATION' => "Bearer #{@authentication_token}"
-                                          }
+    get url_for([:me, :api, :users]), headers: {
+      'CONTENT_TYPE' => 'application/vnd.quicktask.v1+json',
+      'HTTP_AUTHORIZATION' => "Bearer #{@authentication_token}"
+    }
 
     expect(response.headers['Content-Encoding']).not_to be
     content_length = response.headers['Content-Length']
 
-    get url_for([:me, :api, :users]), headers: { 'CONTENT_TYPE' => 'application/vnd.quicktask.v1+json',
-                                            'HTTP_ACCEPT_ENCODING' => 'gzip',
-                                            'HTTP_AUTHORIZATION' => "Bearer #{@authentication_token}"
-                                          }
+    get url_for([:me, :api, :users]), headers: {
+      'CONTENT_TYPE' => 'application/vnd.quicktask.v1+json',
+      'HTTP_ACCEPT_ENCODING' => 'gzip',
+      'HTTP_AUTHORIZATION' => "Bearer #{@authentication_token}"
+    }
 
     expect(response.headers['Content-Encoding']).to eq('gzip')
     expect(response.headers['Content-Length']).not_to eq(content_length)
@@ -26,7 +28,7 @@ RSpec.describe Rack::Deflater, type: :request do
     user = create(:user)
     workspace = create(:workspace, :with_roles, owner: user, name: 'Subscribed Space')
     role = workspace.roles.find_by(permission_level: :owner)
-    team_member = create(:team_member, workspace: workspace)
+    create(:team_member, workspace: workspace)
     create(:workspace_user, role: role, user: user, workspace: workspace)
     token = create(:token, user: user)
     @authentication_token = token_for(user_id: user.id, token_id: token.id)

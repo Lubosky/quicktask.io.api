516435b5cea5e41c16d844dc85f09517dc88f5bd
Lubos Hricak <lubos@hricak.sk>
2018-12-15 23:06:17 +0100

    Updated CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
     resource '/api/*',
-      headers: ['Authorization', 'Content-Type', 'If-None-Match'],
+      headers: ['Authorization', 'Content-Type', 'If-None-Match', 'QUICKTASK-VERSION', 'QUICKTASK-CLIENT'],
       methods: [:get, :post, :delete, :put, :patch, :options, :head],
       expose: ['WWW-Authenticate', 'Server-Authorization']
   end
